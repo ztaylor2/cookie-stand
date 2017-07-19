24f0ed54a3e9a16f4088ae10083a84d9151f0482
Zach Taylor <ztaylor2@zagmail.gonzaga.edu>
2017-07-19 13:35:05 -0700

    stored table DOM element as a variable

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 
 var hours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm', '8:00pm'];
 
+var storeList = document.getElementById('storeList');
+
 // create header row
 
 function header() {
@@ -143,6 +145,5 @@ function footer() {
 
   storeList.appendChild(trEl);
 }
-
 
 footer();
